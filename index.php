8ef8274751d33e45b417067af11d808b76425c98
Mariam Samy Badr <114897239+mariamsamybadr@users.noreply.github.com>
2023-03-24 22:51:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+phpinfo();
+
+  echo 'Welcome in PHP scripting language';
+  echo '<br>';
+
+  ECHO 'We Love PHP';
+  ECHO '<br>';
+
+  print 'We Love PHP';
+  print '<br>';
+
+  PRINT 'We Love PHP';
+  PRINT '<br>';
+
+?>
+
+<?='We Love PHP With Short Tag';?>
